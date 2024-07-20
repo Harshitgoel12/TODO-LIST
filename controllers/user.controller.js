b7f09536f5259d3c96eabf08e9cf108dddef43cf
Harshitgoel12 <harshit.goel.bca.2021@mitmeerut.ac.in>
2024-07-20 16:14:42 +0530

    second commit

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,4 +4,42 @@ const userhandler=async(req,res)=>{
       const d=await model.find({})
   res.render("index",{d});
 }
-module.exports=userhandler
+
+const createlist=async(req,res)=>{
+    const {title,discription}=req.body;
+    if(!title)res.send("title is required");
+    if(!discription)res.send("discription is required");
+    const data=await model.create({
+     title,
+     discription,
+    })
+    const d=await model.find({});
+   res.render("index",{d})
+}
+
+const deletedata=async(req,res)=>{
+    const id=req.params.id;
+       const data=await model.deleteOne({_id:id});
+        const d=await model.find({});
+       res.render("index",{d});
+  }
+
+const openupdatepage=async(req,res)=>{
+    const id=req.params.id;
+      const data=await model.find({_id:id});
+        res.render("update",{data});
+  }
+
+  const update =async(req,res)=>{
+    const id=req.params.id;
+await model.updateOne({ _id: id }, { $set: { title: req.body.title,discription:req.body.discription } });
+res.redirect("/")
+}
+
+module.exports={
+    userhandler,
+    createlist,
+    deletedata,
+    openupdatepage,
+    update,
+}
--- routers/user.router.js
+++ routers/user.router.js
@@ -1,37 +1,14 @@
 const express=require("express");
-const userhandler=require('../controllers/user.controller')
+const controller=require('../controllers/user.controller')
 const model=require("../models/todo.model")
 const router=express.Router();
 
-router.route("/").get(userhandler)
-router.route("/create").post(async(req,res)=>{
-     const {title,discription}=req.body;
-     if(!title)res.send("title is required");
-     if(!discription)res.send("discription is required");
-     const data=await model.create({
-      title,
-      discription,
-     })
-     const d=await model.find({});
-    res.render("index",{d})
-})
-router.route("/delete/:id").post(async(req,res)=>{
-  const id=req.params.id;
-     const data=await model.deleteOne({_id:id});
-      const d=await model.find({});
-     res.render("index",{d});
-})
+router.route("/").get(controller.userhandler)
+router.route("/create").post(controller.createlist)
+router.route("/delete/:id").post(controller.deletedata)
 
-router.route("/update/:id").post(async(req,res)=>{
-  const id=req.params.id;
-    const data=await model.find({_id:id});
-      res.render("update",{data});
-})
-router.route("/updatedata/:id").post(async(req,res)=>{
-                  const id=req.params.id;
-              await model.updateOne({ _id: id }, { $set: { title: req.body.title,discription:req.body.discription } });
-             res.redirect("/")
-})
+router.route("/update/:id").post(controller.openupdatepage)
+router.route("/updatedata/:id").post(controller.update)
 
 
 
